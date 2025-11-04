d2316c8a3ec71fdd02ca1e330d42ffd9871204b0
Kashish Gupta <68956193+Kashish2402@users.noreply.github.com>
2025-11-04 15:14:24 +0530

    Update Header.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -112,14 +112,14 @@ function Header() {
               transition={{ delay: 1.4, duration: 0.5 }}
               className="w-full tracking-normal mx-auto mt-5 flex flex-col md:flex-row items-center gap-8"
             >
-             /* <a
+          {   /* <a
                 href="/Kashish Gupta-Resume.pdf"
                 download="Kashish-Gupta-Resume.pdf"
                 aria-label="download-resume"
                 className="flex w-full md:w-1/2 gap-2 items-center justify-center font-bold cursor-pointer text-base bg-purple-600 py-3 px-5 rounded-xl text-white dark:bg-[rgb(85,75,205)] border-2 border-transparent transition-colors duration-300 hover:bg-transparent hover:text-purple-600 dark:hover:text-[rgb(85,75,205)] hover:border-purple-600 dark:hover:border-[rgb(85,75,205)] "
               >
                 Download Resume <DownloadIcon size={20} />
-              </a>*/
+              </a>*/}
 
               <button
                 aria-label="projects"
